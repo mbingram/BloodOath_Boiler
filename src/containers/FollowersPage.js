8c694eb24dce2bc4b28575a711267f3c5b05e840
Mary Beth <mbingramm@gmail.com>
2021-04-11 14:57:14 -0600

    base app built with readme

---
+++ src/containers/FollowersPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class FollowersPage extends Component {
+
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
