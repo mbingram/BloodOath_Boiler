6d563dd08593c7fe14bde4ca769d0e73561bbf4d
Mary Beth <mbingramm@gmail.com>
2021-04-12 11:09:50 -0600

    background for cult maybe

--- src/containers/Cult.js
+++ src/containers/Cult.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import background from './Blood-Oath-soft.png'
 import FollowerCard from '../components/FollowerCard'
 
 export default function Cult() {
     return (
-        <div>
+        <div className="cult" style={{ backgroundImage: `url(${background})` }}>
             <FollowerCard />
         </div>
     )
